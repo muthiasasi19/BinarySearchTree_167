aaf6e0a371d91a4afb8c84945fef529fce0ef504
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-06-29 13:33:25 +0200

    insert  node ke dalam binary seacrh tree

--- BinarySearchTree_167/BinarySearchTree_167.cpp
+++ BinarySearchTree_167/BinarySearchTree_167.cpp
@@ -30,23 +30,27 @@ public:
 	{
 		Node* newNode = new Node(element, NULL, NULL); // Allocate memory for the new node
 		newNode->info = element; // Assign value to the data field of the new node
-		newNode->leftchild = NULL; // make the left child of the new node point to NULL
-		newNode->rightchild = NULL; // make the right child of thenew node point to NULL
-		
+		newNode->leftchild = NULL; // Make the left child of the new node point to NULL
+		newNode->rightchild = NULL; // Make the right child of the new node point to NULL
+
 		Node* parent = NULL;
 		Node* currentNode = NULL;
 		search(element, parent, currentNode); // Locate the node which will be parent of the node to be inserted
 
-
 		if (parent == NULL) // if the parent is NULL (Tree is empty)
 		{
-			ROOT = newNode;  // Mark the new node as ROOT 
-			return;  // Exit
+			ROOT = newNode; // Mark the new node as ROOT
+			return; // Exit
 		}
 
-		if (element < parent->info); // If the value in the data field of the new node is less than that of the parent
+		if (element < parent->info) // If the value in the data field of the new node is less than that of the parent
 		{
-			parent->leftchild = newNode;   // Make the left child of the parent point to the new node
+			parent->leftchild = newNode; // Make the left child of the parent point to the new node
+		}
+		else if (element > parent->info) // If the value in the data field of the new node is less than that of the parent
+		{
+			parent->rightchild = newNode; // Make the right child of the parent point to the new node
 		}
 	}
+
 };
