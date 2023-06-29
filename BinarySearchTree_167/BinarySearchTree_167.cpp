891d9c5cab525d6c1c147d5e621507ebfd6abdeb
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-06-29 13:44:55 +0200

    membuat method void search

--- BinarySearchTree_167/BinarySearchTree_167.cpp
+++ BinarySearchTree_167/BinarySearchTree_167.cpp
@@ -53,4 +53,20 @@ public:
 		}
 	}
 
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+		// this function searches the currentNode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode; if (element < currentNode->info)
+				if (element < currentNode->info)
+					currentNode = currentNode->leftchild;
+				else
+					currentNode = currentNode->rightchild;
+		}
+	}
+
+	
 };
